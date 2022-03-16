0ca9f276d8dad3252b845f5772769f7ce6cca64d
Denis Knecht <denis.knecht@mail.de>
2022-03-16 14:25:46 +0100

    Added content to tiles

--- src/index.js
+++ src/index.js
@@ -50,42 +50,28 @@ app.use(bodyParser.json());
 
 const tiles = {
     tile1: {
-        title: "Tile #1",
-        content: "Das ist ein Test. Das ist ein Test. Das ist ein Test. Das ist ein Test. Das ist ein Test. Das ist ein Test. Das ist ein Test. Das ist ein Test. Das ist ein Test. Das ist ein Test. Das ist ein Test. Das ist ein Test. Das ist ein Test. ",
-        comments: []
+        title: "Was ist HTML?",
+        content: "Die Hypertext Markup Language (HTML, englisch für Hypertext-Auszeichnungssprache) ist eine textbasierte Auszeichnungssprache zur Strukturierung elektronischer Dokumente wie Texte mit Hyperlinks, Bildern und anderen Inhalten. HTML-Dokumente sind die Grundlage des World Wide Web und werden von Webbrowsern dargestellt. Neben den vom Browser angezeigten Inhalten können HTML-Dateien zusätzliche Angaben in Form von Metainformationen enthalten, z. B. über die im Text verwendeten Sprachen, den Autor oder den zusammengefassten Inhalt des Textes. HTML wird vom World Wide Web Consortium (W3C) und der Web Hypertext Application Technology Working Group (WHATWG) weiterentwickelt. Die aktuelle Version ist seit dem 14. Dezember 2017 HTML 5.2, die bereits von vielen aktuellen Webbrowsern und anderen Layout-Engines unterstützt wird. Auch die Extensible Hypertext Markup Language (XHTML) wird durch HTML5 ersetzt. HTML dient als Auszeichnungssprache dazu, einen Text semantisch zu strukturieren, nicht aber zu formatieren. Die visuelle Darstellung ist nicht Teil der HTML-Spezifikationen und wird durch den Webbrowser und Gestaltungsvorlagen wie CSS bestimmt. Ausnahme sind die als veraltet (englisch deprecated) markierten präsentationsbezogenen Elemente. (Quelle: https://de.wikipedia.org/wiki/Hypertext_Markup_Language)"
     },
     tile2: {
-        title: "Tile #2",
-        content: "Test",
-        comments: []
+        title: "Was ist CSS?",
+        content: "Cascading Style Sheets (englische Aussprache für 'gestufte Gestaltungsbögen'; kurz: CSS) ist eine Stylesheet-Sprache für elektronische Dokumente und zusammen mit HTML und JavaScript eine der Kernsprachen des World Wide Webs. Sie ist ein sogenannter living standard 'lebendiger Standard' und wird vom World Wide Web Consortium (W3C) beständig weiterentwickelt. Mit CSS werden Gestaltungsanweisungen erstellt, die vor allem zusammen mit den Auszeichnungssprachen HTML und XML (zum Beispiel bei SVG) eingesetzt werden. (Quelle: https://de.wikipedia.org/wiki/Cascading_Style_Sheets)"
     },
     tile3: {
-        title: "Tile #3",
-        content: "Test",
-        comments: []
+        title: "Was ist JavaScript (JS)?",
+        content: "JavaScript (kurz JS) ist eine Skriptsprache, die ursprünglich 1995 von Netscape für dynamisches HTML in Webbrowsern entwickelt wurde, um Benutzerinteraktionen auszuwerten, Inhalte zu verändern, nachzuladen oder zu generieren und so die Möglichkeiten von HTML zu erweitern. Heute wird JavaScript auch außerhalb von Browsern angewendet, etwa auf Servern und in Microcontrollern. Der heutige Name der ursprünglich LiveScript genannten Sprache entstand 1996 aus einer Kooperation von Netscape mit Sun Microsystems. Deren Java-Applets, erstellt mit der gleichfalls 1995 veröffentlichten Programmiersprache Java, wurden mithilfe von LiveScript in den Netscape Navigator integriert. Um die Popularität von Java zu nutzen, wurde LiveScript in JavaScript umbenannt, obwohl die beiden Sprachen voneinander unabhängig entwickelt wurden und völlig unterschiedliche Grundkonzepte aufweisen. Der als ECMAScript (ECMA 262) standardisierte Sprachkern von JavaScript beschreibt eine dynamisch typisierte, objektorientierte, aber klassenlose Skriptsprache. Sie wird allen objektorientierten Programmierparadigmen unter anderem auf der Basis von Prototypen gerecht, deren Deklaration ab ECMAScript 6 mit einer Syntax ermöglicht wird, wie sie ähnlich auch bei klassenbasierten Programmiersprachen üblich ist. In JavaScript lässt sich je nach Bedarf objektorientiert, prozedural oder funktional programmieren.(Quelle: https://de.wikipedia.org/wiki/JavaScript)"
     },
     tile4: {
-        title: "Tile #4",
-        content: "Test",
-        comments: []
+        title: "Was ist Svelte?",
+        content: "Svelte ist ein Open-Source-Webframework, das es erlaubt, mithilfe deklarativer Komponenten reaktive Single-Page-Webanwendungen zu erstellen. Anders als andere clientseitige Webframeworks fungiert Svelte als Compiler, der den Quellcode in imperativen Java Script Code umwandelt. Somit verzichtet das Framework auf das Konzept des Virtual DOM und weist eine hohe Effizienz zur Laufzeit auf. (Quelle: https://de.wikipedia.org/wiki/Svelte_%28Framework%29)"
     },
     tile5: {
-        title: "Tile #5",
-        content: "Test",
-        comments: []
+        title: "Wie übergibt man Svelte-Komponenten Argumente mit?",
+        content: "Erstelle zuerst eine <COMPONENT_NAME>.svelte-Datei. Erzeuge ein <script>-Tag. Mit 'export let VARIABLE_NAME;' erstellt man ein Argument. Beim Aufrufen der Komponente in einer anderen Svelte-Datei, muss nun die Variable VARIABLE_NAME angegeben werden. Mit 'export let VARIABLE_NAME = DEFAULT_WERT;' kann man ein Argument optional machen."
     },
 }
 
-const userList = {
-    "123": {
-        user: "Test",
-        content_priority: {
-            "tile1": 2,
-            "tile3": 2
-        },
-        favorites: ["tile1"]
-    }
-};
+const userList = {};
 
 app.get('/content', (req, res) => {
 
